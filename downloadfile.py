6146b6726035b4a2032a5aaed67c3a3742a79c4f
Azeem <chaudhry.azeem0308@gmail.com>
2024-07-15 11:08:37 +0500

    Added a LearnMore option

--- downloadfile.py
+++ downloadfile.py
@@ -1,3 +1,13 @@
 import streamlit as st
 import requests
 
+file_url = "https://raw.githubusercontent.com/AzeemChaudhry/Job-scraper/main/LearnMore.txt"
+def download_file(url):
+    try : 
+        response = requests.get(file_url)
+        response.raise_for_status()
+        return response.text
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error fetching file : {e}")
+        return None
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-
+import downloadfile as df
 st.title("Job Scraper")
 st.write(
     "Lets you search for jobs on specified websites, according to your needs.\n"
 )
+file_url = "https://raw.githubusercontent.com/AzeemChaudhry/Job-scraper/main/LearnMore.txt" #file url to download 
 
-if st.button("Click to learn more"): 
-    st.info("Coming soon!")
+with st.expander("Click to learn more"): 
+    LearnMore = df.download_file(file_url)
+    if LearnMore:
+       st.write(LearnMore)
